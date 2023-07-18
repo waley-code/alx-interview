abd05cee2d35e5abdaad13eabc3a64aa27c05d9c
wale_code <walephlp@gmail.com>
2023-07-18 06:40:38 -0700

    completed log parser algorith

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,42 +1,50 @@
 #!/usr/bin/python3
-"""script that reads stdin line by line and computes metrics"""
+"""
+log parsing
+"""
+
 import sys
-
-# Variables to store the metrics
-total_file_size = 0
-status_code_counts = {}
+import re
 
 
-try:
+def output(log: dict) -> None:
+    """
+    helper function to display stats
+    """
+    print("File size: {}".format(log["file_size"]))
+    for code in sorted(log["code_frequency"]):
+        if log["code_frequency"][code]:
+            print("{}: {}".format(code, log["code_frequency"][code]))
+
+
+if __name__ == "__main__":
+    regex = re.compile(
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
+
     line_count = 0
-    for line in sys.stdin:
-        line = line.strip()
+    log = {}
+    log["file_size"] = 0
+    log["code_frequency"] = {
+        str(code): 0 for code in [
+            200, 301, 400, 401, 403, 404, 405, 500]}
 
-        # Extract the relevant information from the line
-        parts = line.split(' ')
-        if len(parts) != 7:
-            continue
-        status_code = parts[5]
-        file_size = int(parts[6])
+    try:
+        for line in sys.stdin:
+            line = line.strip()
+            match = regex.fullmatch(line)
+            if (match):
+                line_count += 1
+                code = match.group(1)
+                file_size = int(match.group(2))
 
-        # Update the metrics
-        total_file_size += file_size
-        status_code_counts[status_code] = status_code_counts.get(status_code, 0) + 1
+                # File size
+                log["file_size"] += file_size
 
-        line_count += 1
+                # status code
+                if (code.isdecimal()):
+                    log["code_frequency"][code] += 1
 
-        # Print statistics every 10 lines
-        if line_count % 10 == 0:
-            print("Total file size:", total_file_size)
-            sorted_status_codes = sorted(status_code_counts.keys(), key=lambda x: int(x))
-            for code in sorted_status_codes:
-                print(code + ":", status_code_counts[code])
-            print()
-
-except KeyboardInterrupt:
-    # Handle keyboard interruption (CTRL + C)
-    print("Total file size:", total_file_size)
-    sorted_status_codes = sorted(status_code_counts.keys(), key=lambda x: int(x))
-    for code in sorted_status_codes:
-        print(code + ":", status_code_counts[code])
-
+                if (line_count % 10 == 0):
+                    output(log)
+    finally:
+        output(log)
