1b54449e7f9dc1a249a2aaddd1dfd644267b59d8
waley-code <oyewaleadeosun@yahoo.com>
2023-07-26 09:02:09 +0100

    update pycodestyle

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-    a method that determines if a given data set represents a valid UTF-8 encoding.
+    a method that determines if a given data set represents
+    a valid UTF-8 encoding.
     Prototype: def validUTF8(data)
     Return: True if data is a valid UTF-8 encoding, else return False
     A character in UTF-8 can be 1 to 4 bytes long
@@ -27,16 +28,16 @@ def validUTF8(data):
         byte = byte & 0xFF
 
         if remaining_bytes == 0:
-            # If the byte starts with "0", it's a single-byte character (1 byte)
+            # If the byte starts with "0", it's a single-byte (1 byte)
             if (byte >> 7) == 0b0:
                 remaining_bytes = 0
-            # If the byte starts with "110", it's a two-byte character (2 bytes)
+            # If the byte starts with "110", it's a two-byte  (2 bytes)
             elif (byte >> 5) == 0b110:
                 remaining_bytes = 1
-            # If the byte starts with "1110", it's a three-byte character (3 bytes)
+            # If the byte starts with "1110", it's a three-byte  (3 bytes)
             elif (byte >> 4) == 0b1110:
                 remaining_bytes = 2
-            # If the byte starts with "11110", it's a four-byte character (4 bytes)
+            # If the byte starts with "11110", it's a four-byte  (4 bytes)
             elif (byte >> 3) == 0b11110:
                 remaining_bytes = 3
             else:
@@ -49,5 +50,5 @@ def validUTF8(data):
 
             remaining_bytes -= 1
 
-    # All characters in the list form a valid UTF-8 encoding
+    # All s in the list form a valid UTF-8 encoding
     return remaining_bytes == 0
