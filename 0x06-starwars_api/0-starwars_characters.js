7765f91dae4ea3e3111cc8223c8e8493b9308177
waley-code <oyewaleadeosun@yahoo.com>
2023-08-11 02:50:09 +0100

    fix semistandard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const request = require("request");
+const request = require('request');
 const filmUrl = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
 request(filmUrl, (error, response, body) => {
   if (error) {
