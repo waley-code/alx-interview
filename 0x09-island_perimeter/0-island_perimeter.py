f4326207dd9cf0a32499099928b2d0c1953c48aa
waley-code <oyewaleadeosun@yahoo.com>
2023-08-31 09:26:49 +0100

    complete algorithm

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""function def island_perimeter(grid): that returns the perimeter of the island described in grid"""
+
+
+def island_perimeter(grid):
+    """returns the perimeter"""
+    rows = len(grid)
+    cols = len(grid[0])
+    
+    perimeter = 0
+    
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                perimeter += 4  # Initialize with the full perimeter
+                
+                # Check neighbors
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2  # Subtract top neighbor
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2  # Subtract left neighbor
+                    
+    return perimeter
